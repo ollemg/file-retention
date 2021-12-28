1f2416ce0c1e2b8324d2b828fee703e168b690b2
Gabriel de Mello Barbosa <gabrielmello808@gmail.com>
2021-12-28 11:26:10 -0300

    Ajustes para virar modulo

--- file_retention/file_retention.py
+++ file_retention/file_retention.py
@@ -9,74 +9,85 @@ import click
 
 
 @click.command()
-@click.option('--retention','-r', default=15, help='Number of days')
-@click.option('--extension','-e', default="*", help='Extension of files')
+@click.option('--retention','-r', default=15, help='Number of days. Default value 15')
+@click.option('--extension','-e', default="*", help='Extension of files. Default value *')
 @click.option('--path','-p', help='Path')
+@click.option('--delete','-d', default=False ,help='For delete files', is_flag=True)
 
-def file_retention(retention, extension, path):
+def main(retention, extension, path, delete):
     """ Delete files based on predefined dates """
-    space = "---------------------------------------------------------"
+    space = "--------------------------------------------------------------------"
     today = date.today()
     days_ago = today - timedelta(days=retention)
     home = expanduser("~")
-    output_files = os.path.join(home, "file_retention")
-    click.echo(space)
-    click.echo(f'Diretório criado: {output_files}')
-    os.makedirs(output_files, exist_ok = True)
+    output_yaml = os.path.join(home, ".file_retention")
+    if os.path.exists(output_yaml):
+        os.makedirs(output_yaml, exist_ok = True)
+        click.echo(f'{space}\nDiretório criado: {output_yaml}')
+    else:
+        click.echo(f'{space}\nDiretório já existe: {output_yaml}')
 
     def create_yaml(dictionary, date):
-        """ cria o arquivo yaml esperando um dicionario e uma data"""
-        full_path = os.path.join(output_files, f'{date}.yml')
-        with open(full_path, 'w') as yaml_file:
-            yaml.dump(dictionary, yaml_file, default_flow_style=False)
-        click.echo(space)
-        click.echo(f'Arquivo exportado: {full_path}')
+        """ recebe um dicionario e uma data para montar o arquivo yaml"""
+        full_path = os.path.join(output_yaml, f'{date}.yml')
+        if len(dictionary) == 2:
+            click.echo(space)
+            click.echo(f"Não há arquivos no diretorio ou não foi passado um diretorio.")
+        else:
+            with open(full_path, 'w') as yaml_file:
+                yaml.dump(dictionary, yaml_file, default_flow_style=False)
+            click.echo(space)
+            click.echo(f'Arquivo exportado: {full_path}')
 
     def get_files(fullpath):
-        """ Pega os arquivos recursivamente esperando um diretório e uma extensão de arquivo"""
+        """ Recebe um diretorio e coleta todos os arquivos que existe no diretorio recursivamente e salva em uma lista"""
         values = [f for f in glob.glob(f"{fullpath}**/*.{extension}", recursive=True)]
         count = len(values)
-        click.echo(space)
-        click.echo(f'{count} arquivos encontrados!')
+        click.echo(f'{space}\n{count} arquivos encontrados!')
         return values
 
-    def create_dict(date):
-        """ Cria o dicionario com a data e a lista de arquivos encontrado na funcao get_files() """
+    def create_dict(date, fullpath):
+        """ recebe uma data e cria um dicionario com a data e a lista de arquivos encontrados na funcao get_files() """
         dicts = {}
         dicts["date"] = date
-        dicts["arquivos"] = get_files(path)
-        click.echo(space)
-        click.echo(f'Dicionário criado!')
+        dicts["arquivos"] = get_files(fullpath)
         return dicts
 
-    def read_yaml(files, key):
-        """ lê o arquivo yaml e filtra"""
-        full_path = os.path.join(output_files, f'{files}.yml')
-        with open(full_path, "r") as config:
-            data = yaml.load(config, Loader=SafeLoader)
-            data = data[key]
-            click.echo(space)
-            click.echo(f"Lendo o arquivo: {full_path}")
+    def read_yaml(date, key):
+        """Recebe uma data e uma chave para ler o arquivo yaml"""
+        full_path = os.path.join(output_yaml, f'{date}.yml')
+        if os.path.exists(full_path):
+            click.echo(f"{space}\nArquivo {full_path} Encontrado!")
+            with open(full_path, "r") as config:
+                data = yaml.load(config, Loader=SafeLoader)
+                data = data[key]
+                click.echo(f"{space}\nLendo o arquivo: {full_path}")
             return data
+        else:
+            click.echo(f"{space}\nAinda não existe o arquivo {full_path}.")
 
     def delete_yaml(files):
+        ...
         
 
     def delete_files(date):
-        """ delete os arquivos consultando o yaml ~/file_retention/yyyy-mm-dd.yml"""
-        files = read_yaml(date, "arquivos")
-        click.echo(space)
-        click.echo(f"Os arquivos de {retention} dias atrás serão excluídos.")
-        click.echo(space)
-        for f in files:
-            if os.path.exists(f):
-                click.echo(f"Arquivo removido: {f}")
-                os.remove(f"{f}")
+        """Recebe uma data deleta os arquivos consultando o yaml ~/.file_retention/yyyy-mm-dd.yml"""
+        if delete:
+            full_path = os.path.join(output_yaml, f'{date}.yml')
+            if os.path.exists(full_path):
+                files = read_yaml(date, "arquivos")
+                click.echo(f"{space}\nOs arquivos de {retention} dias atrás serão excluídos!!\n{space}")
+                for f in files:
+                    if os.path.exists(f):
+                        os.remove(f"{f}")
+                        click.echo(f"Arquivo removido: {f}")
+                    else:
+                        click.echo(f"O Arquivo {f} não existe mais")
+                click.echo(space)
             else:
-                click.echo(f"O Arquivo {f} não existe mais") 
-        click.echo(space)
+                click.echo(f"{space}\nO arquivo {full_path} não existe ainda.\n{space}")
+        else:
+            click.echo(f"{space}\nNada será deletado.\n{space}")
 
-    create_yaml(create_dict(today), today)
+    create_yaml(create_dict(today, path), today)
     delete_files(days_ago)
-
-file_retention()
